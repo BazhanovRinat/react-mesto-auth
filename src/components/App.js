22bf0dab680d599a4782a98bfdd24d691473a464
Rinat Bazhanov <Bazhanov-rinat@yandex.ru>
2023-06-02 10:10:28 +0300

    develop-mesto-react(auth)

--- src/components/App.js
+++ src/components/App.js
@@ -39,6 +39,7 @@ function App() {
   const navigate = useNavigate()
 
   useEffect(() => {
+    if (loggedIn) {
     api.profileDataInstall()
       .then((data) => {
         setCurrentUser(data)
@@ -54,6 +55,7 @@ function App() {
       .catch((error) => {
         console.log(`${error}`);
       })
+    }
     tokenCheck();
   }, [loggedIn])
 
